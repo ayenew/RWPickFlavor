002e6ec0f160a6970c7e81c1d75c1aa43149ea4e
Ayu <ayenew.berellie@toyota.com>
2019-12-27 13:12:52 -0600

    Initial commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Ayenew on 12/27/19.
+//  Copyright © 2019 AAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
